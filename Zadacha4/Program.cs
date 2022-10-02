5256db739c7466321afec39c0951f65de6297511
Elena <evagon4arova@gmail.com>
2022-10-02 15:29:18 +0500

    Четные числа диапозона

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.Clear();
+
+Console.WriteLine("Введите число N: ");
+
+string insert = Console.ReadLine()!;
+int number = int.Parse(insert);
+int count = 2;
+
+if (number < 1)
+{
+    Console.Write("Неверное число");
+}
+if (number == 1)
+{
+    Console.Write("Нет четных");
+}
+
+do
+{
+    if (count == number - 1)
+    {
+        if (count + 2 > number)
+        {
+            Console.Write(count + " ");
+        }
+    }
+    else if (count < number)
+    {
+        Console.Write(count + ", ");
+    }
+    else if (count == number)
+    {
+        Console.Write(count + " ");
+    }
+    count = count + 2;
+} while (count <= number);
