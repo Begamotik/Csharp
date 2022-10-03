27ca5d316deafb6b9b8e39bcd12e17b31ca6559f
Elena <evagon4arova@gmail.com>
2022-10-04 00:35:36 +0500

    выводит третью цифру заданного числа

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Введите номер дня недели:");
+int number = Input();
+if (number >=1 && number <=5)
+{
+Console.WriteLine($"{DayWeek(number)}!Это будний день");
+}
+else if (number == 6 || number == 7)
+{
+Console.WriteLine($"{DayWeek(number)}!Это выходной");
+}
+else
+{
+Console.WriteLine("Введите значение от 1 до 7");
+}
