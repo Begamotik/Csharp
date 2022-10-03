0b941b25b9049276606483a1e8b04f68dc005a0e
Elena <evagon4arova@gmail.com>
2022-10-04 00:36:33 +0500

    Выходной день недели или нет

---
+++ Zadacha15/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+int InputIntNumber()
+
+Console.Write("Введите номер дня недели:");
+
+int number = int.Parse(Console.ReadLine() ?? "0");
+return number;
+
+int num = InputIntNumber();
+if (num >=1 && num <=5)
+{
+Console.WriteLine("Это будний день");
+}
+else if (num == 6 || num == 7)
+{
+Console.WriteLine("Это выходной");
+}
+else
+{
+Console.WriteLine("Введите значение от 1 до 7");
+}
