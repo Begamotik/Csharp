5c00fde73976d211c27b09fa3d654ddd3593cb56
Elena <evagon4arova@gmail.com>
2022-10-12 06:13:24 +0500

    Палиндром 5значного числа

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число  
+// и проверяет, является ли оно палиндромом.
+
+int InputIntNumber()
+{
+    while (true)
+    {
+        Console.Write("Введите 5значное число: ");
+        int number = int.Parse(Console.ReadLine() ?? "0");
+        while (number > 9999 && number < 100000)
+            return number;
+    }
+    
+}
+
+int num = InputIntNumber();
+
+
+void Polindrom()
+{
+    string chars = num.ToString();
+    if (chars[0] == chars[4] && chars[1] == chars[3])
+    {
+        Console.Write("Число является палиндромом");
+    }
+    else
+    {
+        Console.Write("Число не является палиндромом");
+    }
+}
+
+Polindrom();
