0b40aa4b4eec0e4e3f6a641973e32c5ea261293b
Elena <evagon4arova@gmail.com>
2022-10-17 20:02:06 +0500

    Наименьший общий делитель 2х чисел

---
+++ Zadacha68/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 68: Задайте значения M и N. Напишите программу, которая найдёт наибольший общий делитель (НОД) этих чисел с помощью рекурсии.
+// M = 28; N = 7 -> 7
+
+Console.Clear();
+
+int InputIntNumberM()
+{
+    while (true)
+    try
+        {
+            Console.Write("Введите число M: ");
+            int M = int.Parse(Console.ReadLine() ?? "0");
+            return M;
+        }
+        catch 
+        {
+            Console.WriteLine("Введите целое число, пожалуйста");
+        }
+}
+
+int InputIntNumberN()
+{
+    while (true)
+    try
+        {
+            Console.Write("Введите число N: ");
+            int N = int.Parse(Console.ReadLine() ?? "0");
+            return N;
+        }
+        catch 
+        {
+            Console.WriteLine("Введите целое число, пожалуйста");
+        }
+}
+
+int M = InputIntNumberM();
+int N = InputIntNumberN();
+
+void Recursion(int M, int N)
+{
+    if (M > N)
+    {
+        M = M % N;
+    }
+    else
+    {
+        N = N % M;
+    }
+    if (M != 0 && N != 0)
+    {
+        Recursion(M, N);
+    }
+    else
+    {
+        Console.WriteLine("NOD "+ (M+N).ToString());
+    }
+}
+
+Recursion(M, N);
